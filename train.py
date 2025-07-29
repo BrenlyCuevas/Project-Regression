d602fb41f249db284aa856e7256171d1aeb8e574
BrenlyCuevas <akashi@gmail>
2025-07-28 23:51:12 -0400

    a base code was created for regression with random forests

--- train.py
+++ train.py
@@ -1,8 +1,7 @@
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.datasets import make_classification
-X, y = make_classification(n_samples=1000, n_features=4,
-                           n_informative=2, n_redundant=0,
-                           random_state=0, shuffle=False)
-clf = RandomForestClassifier(max_depth=2, random_state=0)
-clf.fit(X, y)
-print(clf.predict([[0, 0, 0, 0]]))
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.datasets import make_regression
+X, y = make_regression(n_features=4, n_informative=2,
+                       random_state=0, shuffle=False)
+regr = RandomForestRegressor(max_depth=2, random_state=0)
+regr.fit(X, y)
+print(regr.predict([[0, 0, 0, 0]]))
