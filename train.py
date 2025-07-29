f6a4f88bdd5d01a392643dcb4c5c8da8ab64e941
BrenlyCuevas <akashi@gmail>
2025-07-28 23:59:43 -0400

    comments were added

--- train.py
+++ train.py
@@ -1,5 +1,8 @@
+# import libraries
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.datasets import make_regression
+
+# Creating Dataset
 X, y = make_regression(n_features=4, n_informative=2,
                        random_state=0, shuffle=False)
 regr = RandomForestRegressor(max_depth=2, random_state=0)
