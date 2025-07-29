b115b9b6689cec8abc3e703d6e68e27031489b2c
BrenlyCuevas <akashi@gmail>
2025-07-29 00:10:00 -0400

    a created model comment was added

--- train.py
+++ train.py
@@ -5,6 +5,8 @@ from sklearn.datasets import make_regression
 # Creating Dataset
 X, y = make_regression(n_features=4, n_informative=2,
                        random_state=0, shuffle=False)
+
+# Creating and validating model
 regr = RandomForestRegressor(max_depth=2, random_state=0)
 regr.fit(X, y)
 print(regr.predict([[0, 0, 0, 0]]))
